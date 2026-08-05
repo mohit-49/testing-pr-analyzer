ee9e3d8d42fcd48b8364f9f981c5a8ca8cb56bd5
mohit-sharma <mohit.sharma4@dotsquares.com>
2026-08-05 12:45:44 +0530

    add the duplicated data

--- src/components/pages/auth/Cart/cart.tsx
+++ src/components/pages/auth/Cart/cart.tsx
@@ -7,7 +7,7 @@ export default function Cart() {
     
     return (
         <div className="cart-wrapper">
-            <GoogleTranslate />       
+            <GoogleTranslate kl/>       
             <Link href="/">
                 <div className="header-left">
                     <img src="/group1-removebg-preview.png" alt="Logo" className="header-logo" />
